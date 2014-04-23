8512ffaa297c458d04457d7762938dcb868fcf0d
cab404 <cab404@ya.ru>
2014-04-23 15:00:28 +0400

    Используем Tabun.Auth.

--- src/everypony/sweetieBot/activities/Login.java
+++ src/everypony/sweetieBot/activities/Login.java
@@ -1,7 +1,9 @@
 package everypony.sweetieBot.activities;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import com.cab404.libtabun.pages.TabunPage;
@@ -10,8 +12,6 @@ import com.cab404.libtabun.parts.UserInfo;
 import com.cab404.libtabun.util.SU;
 import com.cab404.moonlight.framework.AccessProfile;
 import everypony.sweetieBot.U;
-import everypony.tabun.auth.TabunAccount;
-import everypony.tabun.auth.TabunTokenGetterActivity;
 
 /**
  * Входит за юзера или дает вариант войти readonly.
@@ -24,7 +24,17 @@ public class Login extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Пайпим.
-        startActivityForResult(new Intent(this, TabunTokenGetterActivity.class), TOKEN_REQUEST_CODE);
+        try {
+            startActivityForResult(new Intent("everypony.tabun.auth.TOKEN_REQUEST"), TOKEN_REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            Intent download = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=everypony.tabun.auth")
+            );
+            startActivity(download);
+            setResult(RESULT_CANCELED);
+            finish();
+        }
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -32,7 +42,7 @@ public class Login extends Activity {
 
         if (requestCode == 42) {
             if (resultCode == RESULT_OK) {
-                final String token = data.getStringExtra(TabunAccount.COOKIE_TOKEN_TYPE);
+                final String token = data.getStringExtra("everypony.tabun.cookie");
                 U.v(token);
                 new AsyncTask<Void, Void, Void>() {
                     @Override protected Void doInBackground(Void... voids) {
